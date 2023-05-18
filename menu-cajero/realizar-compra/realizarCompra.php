a0fdc27619c6111510fe9d8a5a25aca12e96a027
Nicolas11Garcia <74791232+Nicolas11Garcia@users.noreply.github.com>
2023-05-18 00:12:23 -0400

    Mostrar oferta en realizar compra

--- menu-cajero/realizar-compra/realizarCompra.php
+++ menu-cajero/realizar-compra/realizarCompra.php
@@ -1,3 +1,19 @@
+<?php
+
+include('../../php/class/Dao.php');
+session_start();
+$dao = new DAO();
+
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,7 +32,105 @@
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 
+    <link href="
+  https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
+  " rel="stylesheet">
+
     <link rel="stylesheet" href="../../assets/css/menu-cajero/realizarcompra/realizarcompra.css">
+
+    <style>
+        .rut-ofertas{
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .splide{
+            width: 450px;
+            
+        }
+
+        .contenedor-ofertas{
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            margin-right: 10px;
+        }
+
+        .item-1-oferta{
+            width: 300px;
+            height: 200px;
+            margin-right: 10px;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 20px;
+
+            border: 1px solid #51AA1B;
+        }
+
+        .contenedor-img-oferta{
+            width: 100px;
+        }
+
+        .contenedor-img-oferta img{
+            width: 100%;
+        }
+
+        .textos{
+            text-align: center;
+        }
+
+        .texto-oferta{
+            font-size: 14px;
+            font-weight: 600;
+            margin-bottom: 7px;
+        }
+
+        .oferta-precio{
+
+            margin-bottom: 7px;
+            font-size: 24px;
+            font-style: italic;
+        }
+
+        .condicion-oferta{
+            margin-bottom: 7px;
+            font-size: 14px;
+        }
+
+        .pecio-normal{
+            font-size: 14px;
+            color: #707070;
+        }
+
+        .splide__pagination__page.is-active{
+            background: #51AA1B;
+        }
+
+        ::-webkit-scrollbar {
+            display: none;
+        }
+
+        .contenedor-img-oferta-flex{
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+        }
+
+        .contenedor-img-oferta-flex img{
+            width: 70px;
+        }
+
+        .descuentoporcentaje{
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 600;
+            max-width: 165px;
+        }
+
+    </style>
+
 
     <title>Menu cajero - Kala</title>
 </head>
@@ -36,28 +150,131 @@
     </div>
 
     <form>
-        <div class="input-normal">
-            <label class="label-input">Rut:</label>
-            <div class="flex-rut-botones">
-                <input class="input-text" type="text" id="rut" onkeyup="formatRut(this)" placeholder="Ingrese Rut del cliente" maxlength="12">
-                <button type="button" class="btn btn-iniciar-compra margin-btn btnhover" id="iniciar-compra">Iniciar compra</button>
+        <div class="rut-ofertas" id="rut-ofertas">
+            <div class="input-normal">
+                <label class="label-input">Rut (Puntos y guion se generan automaticamente):</label>
+                <div class="flex-rut-botones">
+                    <input class="input-text" type="text" id="rut" onkeyup="formatRut(this)" placeholder="Ingrese Rut del cliente" maxlength="12">
+                    <button type="button" class="btn btn-iniciar-compra margin-btn btnhover" id="iniciar-compra">Iniciar compra</button>
+                </div>
             </div>
+
+            <div class="contenedor-ofertas" id="ofertas">
+                <div class="splide" id="splideuno" aria-labelledby="carousel-heading">
+                    <div class="splide__track">
+                        <ul class="splide__list">
+
+                            <?php
+                            $lista_ofertas = $dao->mostrarOferta1Producto();
+
+                            $porcentaje_restante = 100;
+                            $precio_oferta_descuento = 0;
+
+                            foreach($lista_ofertas as $k){
+                                $porcentaje_restante = $porcentaje_restante - $k->getPorcentaje();
+
+                                echo '
+                                <div class="splide__slide item-1-oferta">
+                                    <div class="contenedor-img-oferta">
+                                        <img src="../../assets/imagenes/Productos/'.$k->getImagen().'">
+                                    </div>
+                                    <div class="textos">
+                                        <p class="texto-oferta">Precio Oferta</p>
+                                        ';
+
+                                        if($k->getOferta() == 1){
+                                            $precio_oferta_descuento = $k->getPrecioOferta() * $porcentaje_restante / 100;
+                                        }
+
+                                        else{
+                                            $precio_oferta_descuento = $k->getPrecioVenta() * $porcentaje_restante / 100;
+                                        }
+
+                                        echo '
+
+                                        <p class="oferta-precio">$'.number_format($precio_oferta_descuento, 0, ',', '.').'</p>
+                                        <p class="condicion-oferta">Llevando '.$k->getCantidad().' unidades</p>
+                                        <p class="pecio-normal">Precio normal: '.number_format($k->getPrecioVenta(), 0, ',', '.').'</p>
+                                    </div>
+                                </div>
+                                
+                                ';
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="splide" id="splidedos" aria-labelledby="carousel-heading">
+                    <div class="splide__track">
+                        <ul class="splide__list">
+                            <?php
+
+                            $lista_2_productos_ofertas = $dao->mostrarOferta2ProductosAll();
+
+                            foreach($lista_2_productos_ofertas as $k){
+                                echo '
+                                <div class="splide__slide item-1-oferta">
+                                    <div class="contenedor-img-oferta contenedor-img-oferta-flex">
+
+                                
+                                ';
+                                $lista_img_producto_1 = $dao->mostrarProductoPorId($k->getFkProducto1());
+                                $lista_img_producto_2 = $dao->mostrarProductoPorId($k->getFkProducto2());
+
+                                foreach($lista_img_producto_1 as $l){
+                                    echo '
+                                        <img src="../../assets/imagenes/Productos/'.$l->getImagen().'">';
+                                
+                                foreach($lista_img_producto_1 as $l){
+                                    echo '
+                                        <img src="../../assets/imagenes/Productos/1.jpg">
+                                        
+                                        </div>';
+
+                                }
+
+                                echo '
+                                    <div class="textos">
+                                        <p class="oferta-precio descuentoporcentaje">%'.$k->getPorcentaje().' de descuento Si llevas ambos productos</p>
+                                    </div>
+                                </div>
+                                    ';
+
+                                }
+                                
+                            }
+
+                            ?>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
+
         <div class="contenedor-agregar-cliente oculto" id="contenedor-agregar-cliente">
-            <div class="flex-input">
+            <div class="flex-input" style="gap: 30px;">
                 <div class="input-normal">
                     <label class="label-input">Nombre:</label>
-                    <input class="input-text" type="text" id="nombre" placeholder="Ingrese nombre del cliente">
+                    <input style="width: 310px;" class="input-text" type="text" id="nombre" placeholder="Ingrese nombre del cliente">
                 </div>
     
                 <div class="input-normal">
                     <label class="label-input">Apellido:</label>
-                    <input class="input-text" type="text" id="apellido" placeholder="Ingrese apellido del cliente">
+                    <input style="width: 310px;" class="input-text" type="text" id="apellido" placeholder="Ingrese apellido del cliente">
+                </div>
+
+                <div class="input-normal">
+                    <label class="label-input">Correo:</label>
+                    <input style="width: 310px;" class="input-text" type="text" id="correo" placeholder="Ingrese correo del cliente">
                 </div>
             </div>
     
-            <div class="flex-button">
+            <div class="flex-button" style="z-index: 100;">
                 <button class="btn" type="button" id="registrar-e-iniciar">Registrar e Iniciar compra</button>
                 <button type="button" class="btn-cancelar" id="omitir-ingreso-cliente">Omitir</button>
             </div>
@@ -78,12 +295,39 @@
 
 
 
+    <script src="
+    https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js
+    "></script>
+
+<script>
+var splide = new Splide( '#splideuno', {
+  type    : 'loop',
+  perPage : 1,
+  autoplay: true,
+} );
+
+splide.mount();
+
+var splide2 = new Splide( '#splidedos', {
+  type    : 'loop',
+  perPage : 1,
+  autoplay: true,
+} );
+
+splide2.mount();
+</script>
+
+
+
+
+
+
+
 
 
 <script>
     $('#iniciar-compra').click(function(){
         var rut = document.getElementById('rut').value;
-        if (validarRut(rut)) {
             $.ajax({
             type:'POST',
             url:'php/ingresoRut.php',
@@ -96,6 +340,14 @@
                 }
                 else{
                 const contenedor_agregar_cliente = document.getElementById("contenedor-agregar-cliente");
+                
+
+                
+                $('#rut-ofertas').height('100px');
+
+                $("#ofertas").addClass("oculto");
+                $("#ofertas").removeClass("visible");
+
                 $("#contenedor-agregar-cliente").removeClass("oculto");
                 $("#contenedor-agregar-cliente").addClass("visible");
                 $("#iniciar-compra").addClass("oculto");
@@ -106,17 +358,6 @@
                 }
             }
             });
-        } else {
-                    Swal.fire({
-                      title: 'El RUT ingresado no es válido, intente nuevamente.',
-                      icon: 'error',
-                      
-                      allowOutsideClick: false,
-                      showConfirmButton: true,
-                      confirmButtonText: 'Aceptar',
-                      confirmButtonColor: '#61C923',
-                    });
-        }
     });
 
     $('#omitir-ingreso-cliente').click(function(){
@@ -129,6 +370,9 @@
         $("#advertencia-aviso").addClass("oculto");
         $("#advertencia-aviso").removeClass("advertencia");
         $("#advertencia-aviso").css("height","0px");
+
+        $("#ofertas").removeClass("oculto");
+        $("#ofertas").addClass("visible");
     });
 
     $('#registrar-e-iniciar').click(function(){
@@ -136,11 +380,12 @@
         if (validarRut(rut)) {
         var nombre = document.getElementById('nombre').value;
         var apellido = document.getElementById('apellido').value;
+        var correo = document.getElementById('correo').value;
 
             $.ajax({
                 type:'POST',
                 url:'php/ingresarCliente.php', //URL
-                data: {rut,nombre,apellido},
+                data: {rut,correo,nombre,apellido},
                 success: function(res){
                     respuesta = res.trim();
 
@@ -151,16 +396,20 @@
             });
         }
         else {
-                    Swal.fire({
-                      title: 'El RUT ingresado no es válido, intente nuevamente.',
-                      icon: 'error',
+            darOpacidad();
+
+            Swal.fire({
+            title: 'El RUT ingresado no es válido, intente nuevamente.',
+            icon: 'error',
                       
-                      backdrop: false,
-                      allowOutsideClick: false,
-                      showConfirmButton: true,
-                      confirmButtonText: 'Aceptar',
-                      confirmButtonColor: '#61C923',
-                    });
+            showConfirmButton: true,
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#61C923',
+            }).then((result) => {
+                    if (result.isConfirmed) {
+                        ocultarOpacidad();
+                    }
+                });
         }
     });
 
@@ -222,6 +471,31 @@
             }
         }
     }
+
+
+
+
+
+
+        function darOpacidad(){
+            var backdrop = parent.document.getElementById('backdrop');
+            var backdrop_up = parent.document.getElementById('backdrop-up');
+            var backdrop_right = parent.document.getElementById('backdrop-right');
+
+            backdrop.classList.add('visible');
+            backdrop_up.classList.add('visible');
+            backdrop_right.classList.add('visible');
+        }
+
+        function ocultarOpacidad(){
+            var backdrop = parent.document.getElementById('backdrop');
+            var backdrop_up = parent.document.getElementById('backdrop-up');
+            var backdrop_right = parent.document.getElementById('backdrop-right');
+
+            backdrop.classList.remove('visible');
+            backdrop_up.classList.remove('visible');
+            backdrop_right.classList.remove('visible');
+        }
 
     
 </script>
